31ea694628a8fa95db5118c5e186273d64fff54b
Andrẽ Marks <andrem4rks@gmail.com>
2023-01-05 16:35:17 -0500

    cria booking model

---
+++ tdd-study-case-michelli-brito/src/main/java/com/example/tdd/TddStudyCaseMichelliBritoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddStudyCaseMichelliBritoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TddStudyCaseMichelliBritoApplication.class, args);
+  }
+
+}
